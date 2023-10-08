529760e5dad89f20e3a8e2e2b53d0ab95b53a311
Júlia Carneiro <jcgsouza@ecomp.uefs.br>
2023-10-08 02:27:13 -0300

    Extraindo globo/sus

--- Crawler/src/crawler.py
+++ Crawler/src/crawler.py
@@ -15,20 +15,20 @@ class Crawler:
             "https://datasus.saude.gov.br/categoria/noticias/"
         )
         ul_element = raw_datasus.find("ul", {"id": "posts-list"})
-        news = ul_element.find_all("li", {"class": "post"})
+        news_list = ul_element.find_all("li", {"class": "post"})
 
         all_data = []
-        if news is None:
+        if news_list is None:
             if not retry:
                 # time.sleep(3)
                 self.extract_from_datasus(retry=True)
         else:
-            for _news in news:
-                titulo = _news.find("h2")
-                resumo = _news.find("p")
-                raw_data_publicacao = _news.find("span", class_="details")
+            for news in news_list:
+                titulo = news.find("h2")
+                resumo = news.find("p")
+                raw_data_publicacao = news.find("span", class_="details")
                 data_publicacao = raw_data_publicacao.text.strip().split(",")[0]
-                link = _news.find("h2").find("a")
+                link = news.find("h2").find("a")
 
                 data = {
                     "titulo": titulo.text,
@@ -39,7 +39,29 @@ class Crawler:
                 all_data.append(data)
             # print(all_data)
 
+    def extract_from_globo(self):
+        raw_globo = self.request_data("https://g1.globo.com/tudo-sobre/sus/")
+        news_list = raw_globo.find_all("div", class_="feed-post-body")
+
+        all_data = []
+        for news in news_list:
+            titulo = news.find("h2")
+            resumo = news.find("div", class_="feed-post-body-resumo")
+            raw_data_publicacao = news.find("span", class_="feed-post-datetime")
+            data_publicacao = raw_data_publicacao.text.strip().split(",")[0]
+            link = news.find("h2").find("a")
+
+            data = {
+                "titulo": titulo.text,
+                "resumo": resumo.text,
+                "data_publicacao": data_publicacao,
+                "link": link.attrs["href"],
+            }
+            all_data.append(data)
+        # print(all_data)
+
 
 if __name__ == "__main__":
     crawler = Crawler()
     crawler.extract_from_datasus()
+    crawler.extract_from_globo()
